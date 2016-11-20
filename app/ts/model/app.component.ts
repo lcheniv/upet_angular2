37bdbe9836ed11582c91b0d06151b5a3a86eb500
Lawrence Chen <the_Crimson_chen@yahoo.com>
2016-11-20 18:43:36 -0500

    Created model and view folders

--- app/ts/model/app.component.ts
+++ app/ts/model/app.component.ts
@@ -5,7 +5,7 @@ import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',                            // Basically creating our own tag
-    templateUrl: 'app/ts/app.component.html', // Data-binding - to include something from our class in our template which is done by double squigglys {{}}
+    templateUrl: 'app/ts/view/app.component.html',      // Data-binding - to include something from our class in our template which is done by double squigglys {{}}
     directives: [PlaylistComponent]                // Set up directives - telling Angular we are making custom html
 })
 
--- app/ts/model/playlist.component.ts
+++ app/ts/model/playlist.component.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
 
     selector: 'playlist',                               // <playlist> tag
-    templateUrl: 'app/ts/playlist.component.html', // HTML file that represents HTML we want to use for the template
+    templateUrl: 'app/ts/view/playlist.component.html', // HTML file that represents HTML we want to use for the template
     inputs: ['videos']                                  // We have incoming data that needs to be handled by specifiyng its inputs - info coming in
                                                         // Accept data from app component
     
